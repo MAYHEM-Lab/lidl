ee5c6b0f94fdc03e4015ed0b242dee5948a96d14
Fatih Bakir <mfatihbakir@gmail.com>
2020-02-20 00:05:47 -0800

    union usage

--- examples/union.cpp
+++ examples/union.cpp
@@ -1,5 +1,11 @@
 #include "union_generated.hpp"
+#include <iostream>
 
 int main() {
-    procedures p(8);
+		std::array<uint8_t, 128> buf;
+		lidl::message_builder builder(buf);
+		auto& p = lidl::create<procedures>(builder, 42);	
+		auto& p_str = lidl::create<procedures>(builder, lidl::create_string(builder, "foo"));	
+	  std::cout << p.index() << '\n';
+	  std::cout << p_str.index() << '\n';
 }
