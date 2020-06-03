f8f38b263c36b16ae7bddfb05c462aa8c3c2f161
Fatih Bakir <mfatihbakir@gmail.com>
2020-06-03 01:29:58 -0700

    Struct serdes now respects the trailing padding between members and end of struct

--- src/structure.cpp
+++ src/structure.cpp
@@ -1,4 +1,5 @@
 #include <lidl/structure.hpp>
+#include <cassert>
 
 namespace lidl {
 bool structure::is_reference_type(const module& mod) const {
@@ -28,19 +29,34 @@ std::pair<YAML::Node, size_t> structure::bin2yaml(const module& mod,
                                                   gsl::span<const uint8_t> span) const {
     YAML::Node node;
 
-    auto cur_span = span;
+    auto l = layout(mod);
+
+    auto span_size = span.size();
+    std::cerr << span_size << '\n';
+    auto struct_begin = span.size() - l.get().size();
+
     for (auto it = members.rbegin(); it != members.rend(); ++it) {
         auto& [name, member] = *it;
+
+        /**
+         * We have to compute the exact end of the member here due to possible padding.
+         * Our layout keeps track of the offsets of our members.
+         */
+        auto member_layout = get_type(mod, member.type_)->wire_layout(mod);
+
+        auto member_begin_offset = l.offset_of(name).value();
+        auto actual_end = member_begin_offset + member_layout.size();
+        auto cur_span = span.subspan(0, struct_begin + actual_end);
+
         node[name] = get_type(mod, member.type_)->bin2yaml(mod, cur_span).first;
-        cur_span = cur_span.subspan(
-            0, cur_span.size() - get_type(mod, member.type_)->wire_layout(mod).size());
     }
 
     return {node, wire_layout(mod).size()};
 }
+
 int structure::yaml2bin(const module& mod,
-                         const YAML::Node& node,
-                         ibinary_writer& writer) const {
+                        const YAML::Node& node,
+                        ibinary_writer& writer) const {
     std::unordered_map<std::string, int> references;
     for (auto& [mem_name, mem] : members) {
         auto t = get_type(mod, mem.type_);
@@ -53,20 +69,33 @@ int structure::yaml2bin(const module& mod,
     }
 
     writer.align(wire_layout(mod).alignment());
-    auto pos = writer.tell();
+    auto struct_pos = writer.tell(); // Struct beginning
+    auto l = layout(mod);
     for (auto& [mem_name, mem] : members) {
         auto t = get_type(mod, mem.type_);
         if (t->is_reference_type(mod)) {
             const auto pos = references[mem_name];
             YAML::Node ptr_node(pos);
             writer.align(t->wire_layout(mod).alignment());
+
+            auto expected_offset = l.offset_of(mem_name).value();
+            auto cur_offset = writer.tell() - struct_pos;
+            assert(expected_offset == cur_offset);
+
             t->yaml2bin(mod, ptr_node, writer);
         } else {
             auto& elem = node[mem_name];
             writer.align(t->wire_layout(mod).alignment());
+
+            auto expected_offset = l.offset_of(mem_name).value();
+            auto cur_offset = writer.tell() - struct_pos;
+            assert(expected_offset == cur_offset);
+
             t->yaml2bin(mod, elem, writer);
         }
     }
-    return pos;
+    // Must add the trailing padding bytes!
+    writer.align(wire_layout(mod).alignment());
+    return struct_pos;
 }
 } // namespace lidl
--- src/tools/bin2yaml_main.cpp
+++ src/tools/bin2yaml_main.cpp
@@ -22,8 +22,8 @@ int main(int argc, char** argv) {
     reference_type_pass(mod);
     union_enum_pass(mod);
     auto root = std::get<const type*>(get_symbol(*recursive_name_lookup(*mod.symbols, argv[2])));
-    std::ifstream datafile(argv[3]);
-    std::vector<uint8_t> data(std::istream_iterator<uint8_t>(datafile), std::istream_iterator<uint8_t>{});
+    std::ifstream datafile(argv[3], std::ios::binary);
+    std::vector<uint8_t> data(std::istreambuf_iterator<char>(datafile), std::istreambuf_iterator<char>{});
     auto [yaml, consumed] = root->bin2yaml(mod, data);
     std::cout << yaml << '\n';
 }
