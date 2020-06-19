1106f266b9e5dd52e922e7656560e359e25d1cb2
Fatih Bakir <mfatihbakir@gmail.com>
2020-06-19 10:54:28 -0700

    [core] Use .value on optional

--- src/reference_type_pass.cpp
+++ src/reference_type_pass.cpp
@@ -18,7 +18,7 @@ name pointerify(const symbol_handle& ptr_sym, const name& n) {
 }
 
 void reference_type_pass(const module& mod, name& n) {
-    auto ptr_sym = *recursive_name_lookup(*mod.symbols, "ptr");
+    auto ptr_sym = recursive_name_lookup(*mod.symbols, "ptr").value();
     auto member_type = get_type(mod, n);
     if (!member_type->is_reference_type(mod)) {
         return;
