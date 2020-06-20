5c9004b0d7e3eb215e0aec9d3fa44f4f88d781cf
Fatih Bakir <mfatihbakir@gmail.com>
2020-06-19 19:13:06 -0700

    fix examples

--- examples/strings.cpp
+++ examples/strings.cpp
@@ -8,12 +8,12 @@
 int main() {
     std::array<uint8_t, 64> x;
     lidl::message_builder builder(x);
-    auto& p = lidl::create<person>(builder,
+    auto& p = lidl::create<module::person>(builder,
         lidl::create_string(builder, "foo"),
         lidl::create_string(builder, "bar"));
 
-    std::cout << p.name().unsafe_string_view() << '\n'
-              << p.surname().unsafe_string_view() << '\n';
+    std::cout << p.name().string_view() << '\n'
+              << p.surname().string_view() << '\n';
 
     std::cout << "message took " << builder.size() << " bytes\n";
 }
