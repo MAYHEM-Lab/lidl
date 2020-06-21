50b30e3922f4b9333c01f5e4b83572d448a4b3d3
Fatih Bakir <mfatihbakir@gmail.com>
2020-06-21 14:51:37 -0700

    Fix union example

--- examples/union.cpp
+++ examples/union.cpp
@@ -1,11 +1,13 @@
 #include "union_generated.hpp"
+
 #include <iostream>
 
 int main() {
-		std::array<uint8_t, 128> buf;
-		lidl::message_builder builder(buf);
-		auto& p = lidl::create<procedures>(builder, 42);	
-		auto& p_str = lidl::create<procedures>(builder, lidl::create_string(builder, "foo"));	
-	  std::cout << lidl::nameof(p.alternative()) << '\n';
-	  std::cout << lidl::nameof(p_str.alternative()) << '\n';
+    std::array<uint8_t, 128> buf;
+    lidl::message_builder builder(buf);
+    auto& p = lidl::create<module::procedures>(builder, 42);
+    auto& p_str =
+        lidl::create<module::procedures>(builder, lidl::create_string(builder, "foo"));
+    std::cout << lidl::nameof(p.alternative()) << '\n';
+    std::cout << lidl::nameof(p_str.alternative()) << '\n';
 }
