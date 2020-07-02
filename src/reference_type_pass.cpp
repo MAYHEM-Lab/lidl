8db6610c5ce63d9cb6d809836a9e4a40561a58e3
Fatih Bakir <mfatihbakir@gmail.com>
2020-07-01 21:53:45 -0700

    [core] Fix generics of reference types

--- src/reference_type_pass.cpp
+++ src/reference_type_pass.cpp
@@ -7,7 +7,6 @@
 #include <lidl/module.hpp>
 
 
-
 namespace lidl {
 namespace {
 name pointerify(const symbol_handle& ptr_sym, const name& n) {
@@ -18,7 +17,7 @@ name pointerify(const symbol_handle& ptr_sym, const name& n) {
 }
 
 void reference_type_pass(const module& mod, name& n) {
-    auto ptr_sym = recursive_name_lookup(*mod.symbols, "ptr").value();
+    auto ptr_sym     = recursive_name_lookup(*mod.symbols, "ptr").value();
     auto member_type = get_type(mod, n);
     if (!member_type->is_reference_type(mod)) {
         return;
@@ -27,6 +26,18 @@ void reference_type_pass(const module& mod, name& n) {
     if (auto gen = dynamic_cast<const generic_instantiation*>(member_type); gen) {
         if (auto ptr = dynamic_cast<const pointer_type*>(&gen->generic_type()); ptr) {
             // The member is already a pointer
+            return;
+        }
+
+        if (dynamic_cast<const generic_structure*>(&gen->generic_type()) ||
+            dynamic_cast<const generic_union*>(&gen->generic_type())) {
+            /**
+             * The type is a user defined generic type. For instance, my_gen<string>.
+             * We do not wish to expose the internal pointer implementation to the user,
+             * so we don't make the individual arguments pointers, only the entire thing.
+             * i.e. we return ptr<my_gen<string>> rather than ptr<my_gen<ptr<string>>>
+             */
+            n = pointerify(ptr_sym, n);
             return;
         }
 
