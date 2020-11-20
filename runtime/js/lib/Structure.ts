e3b9eb8dc5356aaac99f5b30f779ddf05bf2ca68
Fatih Bakir <mfatihbakir@gmail.com>
2020-11-20 11:14:17 -0800

    [jsrt] Migrate struct base to the new object model, bump version

--- runtime/js/lib/Structure.ts
+++ runtime/js/lib/Structure.ts
@@ -1,17 +1,29 @@
+import {LidlObject, Type, TypedType} from "./Object";
+
 const inspect = Symbol.for('nodejs.util.inspect.custom');
 
-export class StructBase {
-    private _type: any;
-    constructor(type: any) {
-        this._type = type;
-    }
+export type Member = {
+    type: Type;
+    offset: number;
+};
 
-    [inspect](depth: number, opts: any) {
+export interface StructType extends Type {
+    members(): { [key: string]: Member };
+}
+
+export abstract class StructObject extends LidlObject {
+    abstract get_type(): StructType;
+
+    get value(): any {
         const res: any = {};
-        const mems = this._type._metadata.members;
+        const mems = this.get_type().members();
         for (let key in mems) {
             res[key] = (this as any)[key];
         }
         return res;
+    }
+
+    [inspect](depth: number, opts: any) {
+        return this.value;
     }
 }
